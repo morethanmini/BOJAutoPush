c50b31d72b01ac7bcd9dae7cbbce29fb5277aba0
Sang-min, Lee <96113421+morethanmini@users.noreply.github.com>
2022-06-11 14:02:12 +0900

    [Bronze V] Title: 5의 수난, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/23037. 5의 수난/5의 수난.py
+++ 백준/Bronze/23037. 5의 수난/5의 수난.py
@@ -1,5 +1,8 @@
 n = list(input())
+sum = 0
 
-a = (int(n[0]) ** 5) + (int(n[1]) ** 5) +(int(n[2]) ** 5) +(int(n[3]) ** 5) +(int(n[4]) ** 5)
+for i in range(0, 5):
+    a = int(n[i]) ** 5
+    sum += a
 
-print(a)
+print(sum)
