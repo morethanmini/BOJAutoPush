e7cac3ce18394a25f5a5e7624f8fed0a2bc99418
Sang-min, Lee <96113421+morethanmini@users.noreply.github.com>
2022-07-29 15:01:53 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
+++ 프로그래머스/lv1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -2,30 +2,11 @@ def solution(lottos, win_nums):
     count = 0
     zeros = 0
     rank = [6, 6, 5, 4, 3, 2, 1]
-    
-    #for i in range(len(lottos)):
-    #    if lottos[i] == 0:
-    #        zeros += 1
+
     zeros = lottos.count(0)
-    
-    #for i in range(len(lottos)):
-    #    for j in range(len(win_nums)):
-    #        if win_nums[j] == lottos[i]:
-    #            count += 1
     for i in lottos:
         if i in win_nums:
             count += 1
-    
+
     answer = rank[count + zeros], rank[count]
     return answer
-
-
-lottos = [44, 1, 0, 0, 31, 25]
-win_nums = [31, 10, 45, 1, 6, 19]
-#lottos = [0, 0, 0, 0, 0, 0]
-#win_nums = [38, 19, 20, 40, 15, 25]
-#lottos = [45, 4, 35, 20, 3, 9]
-#win_nums = [20, 9, 3, 45, 4, 35]
-
-answer = solution(lottos, win_nums)
-print(answer)
